25fc20169b8cca78c6f8e7e527dc78c0b18c8d65
Malik <hiraev@users.noreply.github.com>
2017-03-19 03:34:48 +0300

    Удалена старая версия.

--- src/Transpose.java
+++ src/Transpose.java
@@ -1,74 +1,36 @@
-import java.util.ArrayList;
+import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-@Deprecated
+
 public final class Transpose {
     private Transpose() {
     }
 
-    public static String[] transpose(final ArrayList<String> lines,
+    public static String[] transpose(@Nonnull final List<String> lines,
                                      final int length,
                                      final boolean trim,
                                      final boolean right) {
-        String[][] stringMatrix;
-        ArrayList<ArrayList<String>> matrix = new ArrayList<>();
-        //Если выравнивание справа, то ставим минус, он добавится в формат строки
+        Map<Integer, String> map = new TreeMap<>();
+        AtomicInteger i = new AtomicInteger(0);
         final String isRight = right ? "" : "-";
-        //Обработка текста и добавление в список
-        int maxLineWidth = 0; //Переменный необходимые для определения размера массива
-        int maxLineHeight = 0; //Хранят максимальное число строк и максимальное число слов в строках
-        for (String line : lines) {
-            int currentMaxLineHeight = 0;
-            final List<String> list = Arrays.stream(line.split(" "))
-                    .filter(e -> !e.equals("") && !e.equals(" "))
-                    .collect(Collectors.toList());
-            ArrayList<String> currentList = new ArrayList<>();
-            for (String word : list) {
-                // Если trim = true и слово длиннее ограничения, то мы его обрезаем
-                if (trim && word.length() > length || word.length() > length) word = word.substring(0, length);
-                // Дополняем строку пробелами
-                if (word.length() < length) word = String.format("%" + isRight + length + "s", word);
-                currentList.add(word);
-                currentMaxLineHeight++;
-            }
-            if (currentMaxLineHeight > maxLineHeight) maxLineHeight = currentMaxLineHeight;
-            matrix.add(currentList);
-            maxLineWidth++;
-        }
-        //Двумерная матрица, размеры которой равны, ширина - кол-во строк входном файле
-        //высота - длина самое длинной строки.
-        stringMatrix = new String[maxLineHeight][maxLineWidth];
-        int i = 0;
-        for (ArrayList<String> outerList : matrix) {
-            int j = 0;
-            for (String string : outerList) {
-                stringMatrix[j][i] = string;
-                j++;
-            }
-            i++;
-        }
-        return toStringArray(stringMatrix, maxLineHeight);
-    }
 
-    //Метод преобразует матрицу строк в массив строк, исключая строки равные null
-    private static String[] toStringArray(String[][] stringMatrix, int maxLineHeight) {
-        String[] lines;
-        lines = new String[maxLineHeight];
-        int i = 0;
-        for (String[] currentLines : stringMatrix) {
-            int j = 0; // Чтобы определить является слово в строке последним
-            StringBuilder stringBuilder = new StringBuilder();
-            for (String word : currentLines) {
-                if (word != null) {
-                    stringBuilder.append(word);
-                    if (j < currentLines.length - 1 && i < currentLines.length - 1) stringBuilder.append(" ");
-                }
-                j++;
-            }
-            lines[i] = stringBuilder.toString();
-            i++;
-        }
-        return lines;
+        lines.stream().map(e -> (Arrays.stream(e.split(" ")))
+                .filter(s -> !s.isEmpty())
+                .map(s -> (trim & s.length() > length) ? s.substring(0, length) : s)
+                .map(s -> (s.length() < length) ? String.format("%" + isRight + length + "s", s) : s)
+                .peek(s -> {
+                    i.getAndIncrement();
+                    if (!map.containsKey(i.get())) map.put(i.get(), "");
+                    //Если добавляемая нами строка первая, то не ставим перед ней пробел, иначе ставим
+                    map.put(i.get(), map.get(i.get()) + ((map.get(i.get()).isEmpty()) ? s : " " + s));
+                })
+                .collect(Collectors.joining())
+        ).forEach(s -> i.set(0));
+
+        return map.values().toArray(new String[map.size()]);
     }
 }
